f8e2de98aed60f87b9414e91a8fab6daec74405e
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-01-04 07:15:07 +0100

    Read user Input

---
+++ MyFirstProject/ConditionalStatement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type something and press the Enter key.");
+            string userValue;
+            userValue = Console.ReadLine();
+            Console.WriteLine("You typed: " + userValue);
+            Console.ReadLine();
+        }
+    }
+}
