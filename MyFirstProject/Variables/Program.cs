6c83afc6f7f9bd03621843d25583ae581f23eae1
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-01-04 06:54:19 +0100

    String Data Type

--- MyFirstProject/Variables/Program.cs
+++ MyFirstProject/Variables/Program.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+ * Data Types
+ * and Variable Declaration
+ * in C#
+ */
+
 namespace Variables
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int x = 7;
-            int y = x + 3;
-            Console.WriteLine(y);
+            /* int x = 7;
+             int y = x + 3;
+             Console.WriteLine(y);
+             Console.ReadLine();
+             */
+
+            string myFirstName;
+            myFirstName = "Bamidele";
+            Console.WriteLine(myFirstName);
             Console.ReadLine();
         }
     }
