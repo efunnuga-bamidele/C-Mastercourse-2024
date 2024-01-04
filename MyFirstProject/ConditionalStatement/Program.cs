85ca1602654cf91e4e379e246aa9f2b968cd5a43
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-01-04 07:25:42 +0100

    Conditional Statement

--- MyFirstProject/ConditionalStatement/Program.cs
+++ MyFirstProject/ConditionalStatement/Program.cs
@@ -10,11 +10,36 @@ namespace ConditionalStatement
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Please type something and press the Enter key.");
             string userValue;
             userValue = Console.ReadLine();
             Console.WriteLine("You typed: " + userValue);
-            Console.ReadLine();
+            */
+
+            Console.WriteLine("Would you prefer what is behing door number 1, 2, or 3?");
+            string userValue = Console.ReadLine();
+            if (userValue == "1")
+            {
+                Console.WriteLine("You won a new car!");
+                Console.ReadLine();
+            }
+            else if (userValue == "2")
+            {
+                Console.WriteLine("You won a new bag!");
+                Console.ReadLine();
+            }
+            else if (userValue == "3")
+            {
+                Console.WriteLine("You won a new boat!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice!");
+                Console.ReadLine();
+            }
+           
         }
     }
 }
