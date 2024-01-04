86d91948e1c40ba36bc5936167fb30abe2ebc060
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-01-04 07:38:30 +0100

    Using Turnery Operator

--- MyFirstProject/ConditionalStatement/Program.cs
+++ MyFirstProject/ConditionalStatement/Program.cs
@@ -17,29 +17,36 @@ namespace ConditionalStatement
             Console.WriteLine("You typed: " + userValue);
             */
 
+        
             Console.WriteLine("Would you prefer what is behing door number 1, 2, or 3?");
             string userValue = Console.ReadLine();
+            /*
+        string message = "";
+        if (userValue == "1")
+        {
+            message = "You won a new car!";
+        }
+        else if (userValue == "2")
+        {
+            message = "You won a new bag!";
+        }
+        else if (userValue == "3")
+        {
+            message = "You won a new boat!";
 
-            string message = "";
-            if (userValue == "1")
-            {
-                message = "You won a new car!";
-            }
-            else if (userValue == "2")
-            {
-                message = "You won a new bag!";
-            }
-            else if (userValue == "3")
-            {
-                message = "You won a new boat!";
-               
-            }
-            else
-            {
-                message = "Sorry, we didn't understand. You loose!";
-            }
+        }
+        else
+        {
+            message = "Sorry, we didn't understand. You loose!";
+        }
 
-            Console.WriteLine(message);
+        Console.WriteLine(message);
+        */
+
+            string message = (userValue == "1") ? "car" : (userValue == "2") ? "boat" : (userValue == "3") ? "bag" : "empty box";
+            Console.WriteLine("Yoy won a/an {0}", message);
+
+
             Console.ReadLine();
 
         }
