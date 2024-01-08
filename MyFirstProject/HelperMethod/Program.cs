ad9f7a13e57df08ff499fd8c16ed6fbec21d3eb4
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-01-08 07:02:19 +0100

    Methods and overload

---
+++ MyFirstProject/HelperMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myValue = superSecretFormula();
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+        }
+
+        private static string superSecretFormula()
+        {
+            string name = greetMe("Bamidele");
+            // some cool stuff here
+            return String.Format("{0}, Hello World!", name);
+        }
+
+        private static string greetMe(string name)
+        {
+            return String.Format("Hello, {0}", name);
+        }
+    }
+}
